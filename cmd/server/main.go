cf1a657d43e6309a0b95a83bd6965813e206714f
xiamingjie <15273155904@163.com>
2022-11-14 21:39:43 +0800

    fix:quic http3 srv

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,11 +16,8 @@ func http3Handle() http.Handler {
 }
 
 func main() {
-	srv := &http3.Server{
-		Addr:           ":2333",
-		Handler:        http3Handle(),
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	log.Fatal(srv.ListenAndServeTLS("./cert/8804910_www.quickstart.top.pem", "./cert/8804910_www.quickstart.top.key"))
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "welcome to quick start")
+	})
+	log.Fatal(http3.ListenAndServeQUIC(":2333", "cert/8804910_www.quickstart.top.pem", "cert/8804910_www.quickstart.top.key", nil))
 }
